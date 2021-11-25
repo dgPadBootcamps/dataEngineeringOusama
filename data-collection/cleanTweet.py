73f8abcfd98f0d30bf487ec5e0e458959b8a9a38
ALMWUSHOOR <ousama.ezz50@gmail.com>
2021-11-25 20:01:39 +0200

    initial commit

---
+++ data-collection/cleanTweet.py
@@ -0,0 +1,9 @@
+import re
+
+
+def clean_tweet(text):
+    text = re.sub('@[A-Za-z 0–9]+', '', text)  # Removing @mentions
+    text = re.sub('#', '', text)  # Removing '#' hash tag
+    text = re.sub('RT[\s]+', '', text)  # Removing RT
+    text = re.sub('https?:\/\/\S+', '', text)  # Removing hyperlink
+    return text
